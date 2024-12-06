4925890d4af0339ff4ebb01be39572cc72557898
FreeAhuraBird <131903454+FreeAhuraBird@users.noreply.github.com>
2024-12-06 22:34:45 +0100

    final generate.py

--- generate.py
+++ generate.py
@@ -1,11 +1,11 @@
 import itertools
 import random
 
-def generate_passwords(chars="abcdefghijklmnopqrstuvwxyz1234567890", max_length=5, filename="five_characters.txt"):
+def generate_passwords(chars="abcdefghijklmnopqrstuvwxyz1234567890", max_length=4, filename="four_characters.txt"):
 
     all_permutations = []
     for length in range(1, max_length + 1):
-        permutations = itertools.permutations(chars, length)
+        permutations = itertools.product(chars, repeat=length)
         all_permutations.extend([''.join(combination) for combination in permutations])
 
     random.shuffle(all_permutations)
